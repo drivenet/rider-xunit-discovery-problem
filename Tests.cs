b570095cc21199057775ba3f7a1391c805e9f950
Aristarkh Zagorodnikov <xm@drive.net>
2018-10-29 15:48:27 +0300

    Initial commit

---
+++ Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AutoFixture.Xunit2;
+using Xunit;
+
+namespace RiderTestProject
+{
+    public class Tests
+    {        
+        [Theory]
+        [InlineData(10)]
+        public void DetectedProperly(int v)
+        {
+            ++v;
+        }
+        
+        [Theory]
+        [AutoData]
+        public void NotDetectedProperly(int v)
+        {
+            ++v;
+        }
+    }
+}
